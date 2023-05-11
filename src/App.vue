a3c2a712bfba2575f3eb2bc037085986b0341790
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-05-11 19:44:56 +0200

    store + saved position list + saved view + filtering

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import PaginationComp from '@/components/PaginationComp.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Pagination</h1>
+    <PaginationComp> </PaginationComp>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
